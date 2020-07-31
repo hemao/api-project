b8437bf32c11ce2d66f935aa6efc3d87f4e2d4e7
HemaO <hema.omprakash@gmail.com>
2020-07-31 19:10:27 -0400

    testing port: 3000

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ const cors = require("cors")
 app.use(cors())
 app.use(require('./lib/routes/index'))
 
-app.set("port", process.env.PORT || 8080)
+
+app.set("port", process.env.PORT || 3000)
 
 app.listen(app.get("port"), () => {
   console.log(`✅ PORT: ${app.get("port")} 🌟`);
 });
+
 
 //app.listen(3000, () => console.log('listening on port 3000'))
 
