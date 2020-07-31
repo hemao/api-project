5cd88e1a2a1edf101cbd9883d7a622eecb07ca50
HemaO <hema.omprakash@gmail.com>
2020-07-31 14:39:32 -0400

    heroku and node setup completed

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 const app = require('express')()
 app.use(require('./lib/routes/index'))
-app.listen(3000, () => console.log('listening on port 3000'))
+
+app.set("port", process.env.PORT || 8080)
+
+app.listen(app.get("port"), () => {
+  console.log(`✅ PORT: ${app.get("port")} 🌟`);
+});
+
+//app.listen(3000, () => console.log('listening on port 3000'))
 
